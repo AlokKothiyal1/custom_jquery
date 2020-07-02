0542d09222d80a12d424e5d3773f9242a5f0c8ac
Alok kothiyal <alokothiyal007@gmail.com>
2020-07-02 20:28:04 +0530

    added toggler

--- custom_jquery.js
+++ custom_jquery.js
@@ -1,22 +1,27 @@
-function JQuery(selector){
-    this.nodes = document.querySelectorAll(selector);
-    this.length = this.nodes.length;
+function Main(selector){
+  this.nodes =document.querySelectorAll(selector)
+  this.length =this.nodes.length
 }
 
-jQuery.prototype.each = function(){
-  // add code
+Main.prototype.each=function(callback){
+  for(var i=0;i<this.length;i++){
+    callback.call(this,this.nodes[i],i)
+  }
+  return this
 }
-
-jQuery.prototype.show = function(){
-  // add code
-}
-
-jQuery.prototype.hide = function(){
-  // add code
+Main.prototype.toggler =function(){
+  for(var i=0;i<this.length;i++){
+    if(this.nodes[i].style.visibility!=='hidden'){
+      this.nodes[i].style.visibility='hidden'
+    }
+    else{
+      this.nodes[i].style.visibility='visible'
+    }
+  }
+  return this
 }
 
 function $(selector){
-    return new jQuery(selector)
+  return new Main(selector);
 }
- 
-export default $
+export default $;
